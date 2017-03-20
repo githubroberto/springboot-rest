d9b80c9251f4556c32adc0279ba468d05f6cdd63
Roberto <githubroberto@users.noreply.github.com>
2017-03-20 21:04:54 +0100

    Added configuration and set context for tests

--- src/test/java/hemma/springboot/stock/ApplicationTest.java
+++ src/test/java/hemma/springboot/stock/ApplicationTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 @WebAppConfiguration
+@ContextConfiguration(classes = {TestConfiguration.class})
 public class ApplicationTest {
 
     @Autowired
